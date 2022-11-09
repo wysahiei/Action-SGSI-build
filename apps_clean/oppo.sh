5e1c2cb122669f0cfeb9c6986d5293d7d690d58c
wysahiei <117861068+wysahiei@users.noreply.github.com>
2022-11-10 04:05:34 +0800

    Update oppo.sh

--- apps_clean/oppo.sh
+++ apps_clean/oppo.sh
@@ -7,27 +7,6 @@ systemdir=$1
 
 apps_dir="
 Email
-OppoCamera
-OppoEngineerCamera
-Browser
-#SogouInput
-HeytapBook
-BrowserVideo
-OPPOStore
-ColorFavorite
-OppoNote2
-OShare
-SOSHelper
-OPPOCommunity
-OperationTips
-KeKeUserCenter
-OppoGallery2t
-KeKePay
-OppoGallery2
-KeKeMarket
-KeKeUserCenter
-GooglePlayServicesUpdater
-GmsCore
 "
 for delete_dir in $apps_dir ;do
   find $systemdir -type d -name "$delete_dir" | xargs rm -rf
